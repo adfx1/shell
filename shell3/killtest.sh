20a8b42805ff449f8ae1e3d02bcf9ee747e0d4e2
wiz@rd <chaoxcores@gmail.com>
2019-11-30 15:56:19 +0800

    add killtest scripts

--- shell3/killtest.sh
+++ shell3/killtest.sh
@@ -7,4 +7,9 @@
 # Description   : 
 ##############################################################################
 
-
+read -p "请输入你要关闭的进程关键词: " process
+pid=($(ps aux |grep $process |awk '{print $2}'))
+for i in ${pid[*]}
+do
+  pkill $i
+done
